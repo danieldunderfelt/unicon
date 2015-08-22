da2bc68d71f5ac0cde2e7798310b5581a7c7b9c6
Dunderfelt, Daniel <daniel.dunderfelt@mirumagency.com>
2015-08-22 20:48:48 +0300

    We've got icons!

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,32 @@
 var gulp = require('gulp')
 var browserSync = require('browser-sync').create()
 var sass = require('gulp-sass')
+var jade = require('gulp-jade')
 var sourcemaps = require('gulp-sourcemaps')
 var autoprefixer = require('gulp-autoprefixer');
+
+var icons = require('./icon-list');
 
 gulp.task('browser-sync', function() {
     browserSync.init({
     	server: {
-    		baseDir: "./build"
+    		baseDir: "./"
     	},
         open: false
     })
+})
+
+gulp.task('jade', function() {
+    var locals = {
+        icons: icons
+    }
+
+    return gulp.src('index.jade')
+        .pipe(jade({
+            locals: locals,
+            pretty: true
+        }).on('error', console.error.bind(console)))
+        .pipe(gulp.dest('.'))
 })
 
 gulp.task('sass', function() {
@@ -18,11 +34,12 @@ gulp.task('sass', function() {
         .pipe(sourcemaps.init())
             .pipe(sass().on('error', sass.logError))
             .pipe(autoprefixer())
-        .pipe(sourcemaps.write())
+        .pipe(sourcemaps.write('.'))
 		.pipe(gulp.dest('./css'))
 		.pipe(browserSync.stream({match: '**/*.css'}))
 })
 
 gulp.task('default', ['browser-sync'], function () {
     gulp.watch(["src/scss/**/*.scss"], ['sass'])
+    gulp.watch(["**/*.jade"], ['jade'])
 })
